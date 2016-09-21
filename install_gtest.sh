6041c983659461afdd29eb28cc7edbdbe9991ed7
Gabriel Tapizquent <gaboth22@gmail.com>
2016-09-20 21:08:11 -0400

    Add cmake and comments

--- install_gtest.sh
+++ install_gtest.sh
@@ -1,17 +1,16 @@
+apt-get install cmake
 apt-get install libgtest-dev
-
+#get files and expand
 wget https://github.com/google/googletest/archive/release-1.7.0.tar.gz
-
 tar xf release-1.7.0.tar.gz
-
+#go into folder
 cd googletest-release-1.7.0
-
+#build code
 cmake -DBUILD_SHARED_LIBS=ON .
-
 make
-
+# "install" library files
 cp -a include/gtest /usr/include
 cp -a libgtest_main.so libgtest.so /usr/lib/
-
+#clean up
 rm -rf googletest-release-1.7.0
 rm release-1.7.0.tar.gz
